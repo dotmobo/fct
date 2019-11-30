3c9fe28fc55cb82c34edb2e6dc45629de16a8959
dotmobo <morgan.bohn@unistra.fr>
2019-11-30 22:42:47 +0100

    change event admon

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Event
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('event_type', 'event_date', 'description')
+    list_display = ('event_type', 'event_date', 'description', 'added_by')
     list_filter = ('event_type',)
 
 admin.site.register(Event, EventAdmin)
--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Event(models.Model):
     ENTRAINEMENT = 'ENTRAINEMENT'
@@ -14,8 +15,10 @@ class Event(models.Model):
         default=ENTRAINEMENT,
         verbose_name = 'Type'
     )
-    event_date = models.DateField(verbose_name = 'Date')
+    event_date = models.DateField(verbose_name = 'Date de l\'événement')
     description = models.CharField(max_length=255, blank=True, verbose_name = 'Description')
+    creation_date = models.DateTimeField(auto_now_add=True, verbose_name = 'Date de création')
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name = 'Ajouté par')
 
     class Meta:
         verbose_name = 'Événement'
