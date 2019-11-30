e1bebba826e0a5656707cc40aa1c31a0c2f19f49
dotmobo <morgan.bohn@unistra.fr>
2019-11-30 23:32:03 +0100

    add attendance model

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from .models import Event
+from .models import Event, Attendance
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ('event_type', 'event_date', 'description', 'added_by')
     list_filter = ('event_type',)
 
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ('event', 'attendee', 'is_attending')
+    list_filter = ('event', 'is_attending')
+
 admin.site.register(Event, EventAdmin)
+admin.site.register(Attendance, AttendanceAdmin)
--- core/models.py
+++ core/models.py
@@ -27,3 +27,13 @@ class Event(models.Model):
         return self.get_event_type_display() + ' du ' + str(self.event_date)
 
 
+class Attendance(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='attendants', verbose_name = 'Événement')
+    attendee = models.ForeignKey(User, on_delete=models.CASCADE, related_name='attending', verbose_name = 'Participant')
+    is_attending = models.BooleanField(null=True, verbose_name = 'Est présent')
+
+    class Meta:
+        verbose_name = 'Présence'
+
+    def __str__(self):
+        return "%s - %s" % (self.event, self.attendee)
