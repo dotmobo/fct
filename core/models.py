b8aab4bfe94cc94836f2df2830374d90a65a7a61
dotmobo <morgan.bohn@unistra.fr>
2019-11-30 21:57:41 +0100

    add event model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    ENTRAINEMENT = 'ENTRAINEMENT'
+    MATCH = 'MATCH'
+    EVENT_TYPES = [
+        (ENTRAINEMENT, 'Entrainement'),
+        (MATCH, 'Match'),
+    ]
+
+    event_type = models.CharField(
+        max_length=50,
+        choices=EVENT_TYPES,
+        default=ENTRAINEMENT,
+    )
+    event_date = models.DateField()
+    description = models.CharField(max_length=255)
+    
+
