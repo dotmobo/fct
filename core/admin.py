590f91d1128ae7bf70da33b76ab36e86e337ebfa
morgan <morgan.bohn@pc-scol.fr>
2019-12-09 21:06:06 +0100

    add profile in admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Event, Attendance
+from .models import Event, Attendance, Profile
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ('event_type', 'event_date', 'description', 'added_by')
@@ -10,5 +10,10 @@ class AttendanceAdmin(admin.ModelAdmin):
     list_display = ('event', 'attendee', 'is_attending')
     list_filter = ('event', 'is_attending')
 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'email_confirmed')
+    list_filter = ('user', 'email_confirmed')
+
 admin.site.register(Event, EventAdmin)
 admin.site.register(Attendance, AttendanceAdmin)
+admin.site.register(Profile, ProfileAdmin)
